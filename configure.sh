0c7038d1caedac741d5095355874b58c82aacf81
kandeel <atkandeel@gmail.com>
2016-06-10 15:26:13 +0100

    Adds script to automatically install dependencies on a clean machine

---
+++ configure.sh
@@ -0,0 +1,6 @@
+sudo apt-get install mysql-server 
+sudo apt-get install libmysqlclient-dev -y
+sudo apt-get install rabbitmq-server -y
+sudo apt-get install python-dev -y
+curl https://bootstrap.pypa.io/get-pip.py | sudo python
+sudo apt-get install supervisor -y
