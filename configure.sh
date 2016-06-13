5c5c224e990038eb2d3e2935364fe77021549393
Ahmed Kandeel <atkandeel@gmail.com>
2016-06-13 14:31:12 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,8 @@
-sudo apt-get install mysql-server 
+sudo apt-get install build-essential -y
+sudo apt-get install mysql-server -y
 sudo apt-get install libmysqlclient-dev -y
 sudo apt-get install rabbitmq-server -y
 sudo apt-get install python-dev -y
 curl https://bootstrap.pypa.io/get-pip.py | sudo python
+sudo apt-get install libyaml-dev libpython2.7-dev -y
 sudo apt-get install supervisor -y
